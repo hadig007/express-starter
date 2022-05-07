6d2f0c3179781ad14ea371c97900582ffe96704f
hadig007 <you@example.com>
2022-05-07 22:28:07 +0700

    starter2

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+// 01-05-2022
+// ini merupakan starter untuk memulai projek express 
+// dibuat oleh hadi gunawan tambunan
+
 const express = require("express");
+const cors = require("cors")
 
 const app = express();
 const URL = 3000;
+
+app.use(cors())
 
 app.listen(URL,()=>console.log(`server start on : http://127.0.0.1:3000`));
